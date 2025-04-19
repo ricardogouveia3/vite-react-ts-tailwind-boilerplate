1a3952605461584d99d054f8eda79f17966ba548
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2025-04-18 22:16:04 -0300

    set pnpm as default; remove non latin char comments; update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,16 +8,13 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    // ES2021 syntax support
     ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
-      // Enable JSX parsing
       jsx: true,
     },
   },
   globals: {
-    // For browser-based globals
     window: 'readonly',
     document: 'readonly',
     Edit: 'writable',
@@ -31,16 +28,13 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:import-x/recommended',
     'plugin:import-x/typescript',
-    // Make prettier as the last item in the extends array, so that it has the opportunity to override other configs
     'plugin:prettier/recommended',
   ],
   settings: {
     react: {
       version: 'detect',
     },
-    // 因为默认的 eslint-plugin-import 只支持 js 和 jsx，对于 ts 和 tsx 需要加下面这两个配置，
     'import-x/parsers': {
-      // 使用 TypeScript parser
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     // https://devrsi0n.com/articles/eslint-typescript-import-unsolve
@@ -59,7 +53,6 @@ module.exports = {
     'prettier/prettier': 'warn',
     'react-hooks/exhaustive-deps': 'warn',
     'react/no-unstable-nested-components': ['warn', { allowAsProps: true }],
-    // 导入类型时需添加 type 修饰符
     '@typescript-eslint/consistent-type-imports': [
       'error',
       {
